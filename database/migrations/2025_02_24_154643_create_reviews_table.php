543cfea8a743f64291cecf9fa9dde6e84e3b3426
Palash Kumer <palashkumer237@gmail.com>
2025-03-01 22:56:59 +0600

    book-review

---
+++ database/migrations/2025_02_24_154643_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+
+            //foreign key
+            // $table->unsignedBigInteger('book_id');
+
+            $table->text('review');
+            $table->unsignedTinyInteger('rating');
+
+            $table->timestamps();
+
+            // Create foreign key
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+
+            //shoter version of above two lines for adding foreign keys
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
